6079eeb831fbfd6df9ce26b57f4a588743c1c653
Christian Wu <wuyirenn@gmail.com>
2025-09-05 12:02:53 -0700

    slightly prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ import { useState, useEffect } from 'react';
 
 export default function Home() {
   return (
-    <main>
-      <h1>Internal Tools</h1>
+    <main className="flex flex-col gap-4 p-4">
+      <h1 className="">Internal Tools</h1>
       <ul>
-        <li>
-          <a href="/cafe-counter">Cafe Counter</a>
+        <li className="">
+          <a className="p-2 text-white border-white border-2 rounded-md hover:bg-white hover:text-black" href="/cafe-counter">Cafe Counter</a>
         </li>
       </ul>
     </main>
